dae8d32c76ece2d62b6f261657a6aef09ac839d0
sushmanarayan7 <sushmanarayan7@gmail.com>
2018-01-08 11:47:48 +0530

    profile phone delete updated

--- src/main/java/AccountActions/userProfilePhoneDelete.java
+++ src/main/java/AccountActions/userProfilePhoneDelete.java
@@ -1,13 +1,19 @@
 package AccountActions;
 
 import base.DriverCreation;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
+
+import static locators.apiKey.apikey;
 
 public class userProfilePhoneDelete extends DriverCreation
 {
     @Test
     public void ProfilePhoneDelete()
     {
-
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response=httpRequest.header("Authorization",apikey).get("/wallet/coin/withdraw/unverified");
     }
 }
